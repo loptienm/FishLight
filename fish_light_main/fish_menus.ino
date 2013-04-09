322233ad0d423f5f476f4bf8b37f8789bc642504
Michael Loptien <loptienm@gmail.com>
2013-04-08 22:55:26 -0600

    FishLight: Add lcd menus file
    Add file that will contain menus for the fish light LCD.  Things like
    setting the time, setting brightness, and similar functions will live
    here.

---
+++ fish_light_main/fish_menus.ino
@@ -0,0 +1,13 @@
+///////////////////////////////
+//
+// This file holds the functions for the Fish LCD Menus.
+// When the middle button is pressed, these are the
+// functions that will be called.
+//
+///////////////////////////////
+
+void get_lcd_menu() {
+  
+}
+
+
