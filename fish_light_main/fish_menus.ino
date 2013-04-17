affd6e88295b7dc7f3a5c334faf67e539a5b65c1
Michael Loptien <loptienm@gmail.com>
2013-04-17 00:04:57 -0600

    FishLight: Add prelim brightness menu functions
    Adding a preliminary function to set the brightness of the LEDs by using
    the buttons.  My idea is to setup a desired number of peaks (0-2 so far)
    where the brightness will be at a maximum.  0 is off always, 1 is to
    slowly ramp up to a consistent brightness, and 2 is to come up, go down,
    and come back up again.  See the code in fish_menus to see what they
    should look like on the LCD screen.  I originally thought that 2 peaks
    would be good for the night light so that I could see the fish very
    easily during 2 peak times but now I'm thinking that is not as useful.
    The blue LEDs are way too bright for their own good.

--- fish_light_main/fish_menus.ino
+++ fish_light_main/fish_menus.ino
@@ -10,4 +10,44 @@ void get_lcd_menu() {
   
 }
 
+/* Peaks example:
+ ----------------
+|Peaks:          |
+|  0  ___________|
+ ----------------
+ ----------------
+|Peaks: /-----\  |
+|  1  _/       \_|
+ ----------------
+ 0123456789012345  <- lcd character position (0 - 15)
+ ----------------
+|Peaks: /\   /\  |
+|  2  _/  \_/  \_|
+ ----------------
+*/
+void set_brightness_buttons() {
+  int numPeaks = 1;  // Assume at least one peak (though 0 is valid)
+
+  print_brightness_peaks_lcd(numPeaks);
+}
+
+void print_brightness_peaks_lcd(int numPeaks) {
+  if (numPeaks == 0) {
+    lcd.clear();
+    lcd.setCursor(0,1); lcd.print("Peaks:          ");
+    lcd.setCursor(0,0); lcd.print("  0  ___________");
+  } else if (numPeaks == 1) {
+    lcd.clear();
+    lcd.setCursor(0,1); lcd.print("Peaks: /-----\  ");
+    lcd.setCursor(0,0); lcd.print("  1  _/       \_");
+  } else if (numPeaks == 2) {
+    lcd.clear();
+    lcd.setCursor(0,1); lcd.print("Peaks: /\   /\  ");
+    lcd.setCursor(0,0); lcd.print("  2  _/  \_/  \_");
+  } else if (numPeaks < 0 || numPeaks > 2) {
+    lcd.clear();
+    lcd.print("ERR: Bad # Peaks");
+  }
+}
+
 
