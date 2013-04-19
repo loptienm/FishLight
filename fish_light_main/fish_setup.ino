1ca9832d557381f9445c30ea8700ca0c14687522
Michael Loptien <loptienm@gmail.com>
2013-04-19 03:03:48 -0600

    FishLight: Make the setup() function more streamlined
    Making the setup() function more streamlined so that it does
    initialization of different things all around the same time.  Also
    adding comments to make it more clear what is going on.

--- fish_light_main/fish_setup.ino
+++ fish_light_main/fish_setup.ino
@@ -6,24 +6,29 @@
 ///////////////////////////////
 
 void setup()  {
+  // Initialize I2C
   Wire.begin();
-  //Serial.begin(115200);
-  lcd.begin(16, 2);
-  lcd.cursor();
-  lcd_cursor_loc[0] = 0;  // Init the cursor location
-  lcd_cursor_loc[1] = 0;
-  lcd.setCursor(lcd_cursor_loc[0],lcd_cursor_loc[1]);
-//  pinMode(A1, OUTPUT);
-  analogWrite(LCDBackLightPin, lcd_use_lvl);
   
-  // Initialize readings
+  // Initialize Serial for 115200 baud
+  Serial.begin(115200);
+  
+  // Initialize the LCD Screen
+  lcd.begin(16, 2);  // Initialize a 16x2 LCD screen
+  lcd.clear();       // Clear anything that might still be on the LCD screen
+  lcd_cursor_loc[0] = 0; // Init the cursor location
+  lcd_cursor_loc[1] = 0;
+  lcd.setCursor(lcd_cursor_loc[0],lcd_cursor_loc[1]);  // Set the cursor location
+  analogWrite(LCDBackLightPin, lcd_use_lvl);  // Turn on the LCD backlight
+  
+  // Initialize potentiometer readings
   for (int thisReading = 0; thisReading < numReadings; thisReading++) {
     readings[thisReading] = analogRead(PotPin) * (255.0 / 1023.0);
     total = total + readings[thisReading];
   }
   average = total / numReadings;
 
-  byte rtc_data = 0;
+  // Set up the Square Wave generator on the RTC
+  //byte rtc_data = 0;
   //RtcWrite(0x07, 0xFF);  // Start the RTC square wave generator for 1Hz
   //rtc_data = RtcRead(0x07);
   //Serial.print("RTC data returned: 0x");
@@ -31,11 +36,20 @@ void setup()  {
   
   // Initialize the time variables
   readDateDS1307();
-  Serial.begin(115200);
-  printCurrentTime();  
   targetnewtime = newtime;
-  lcd.print("Board Starting...");
+  
+  // State that the board is finished initializing
+  lcd.clear();
+  lcd.print("Board Starting!");
   lcd.setCursor(lcd_cursor_loc[0],lcd_cursor_loc[1]);
   Serial.println("Board Initialized, ready for commands.");
+  printCurrentTime();  // Print the current time to the Serial port
   delay(2000);
+  
+  // Turn on the LCD cursor (looks like an underline)
+  // after the lcd has been setup and displays "starting".
+  // This is for aesthetics to not put a cursor on "Board Starting"
+  lcd.cursor();
+  
+  // Start the loop!
 }
